430b7fec766e2101b5240086379501afaf1ee769
SoviljMarko <marko.sovilj@gmail.com>
2026-04-15 14:12:24 +0200

    commit 15/04/2026 - API implementation

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,37 @@
+import io.javalin.Javalin;
 import java.sql.SQLException;
-import java.util.Scanner;
-
 public class Main {
-        
-//    public static void main(String[] args) {
-//
-//        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-//
-//
-//        System.out.println("Enter dice");
-//        String dicesToRoll = myObj.nextLine();
-//
-//        RollerLogic.mainProcessLoop(dicesToRoll); // <---- testiram loop
-////        DiceDecoder.validateAndInitializeDiceRoll(dicesToRoll); <--- ovo je vazece
-//
-//    }
 
     public static void main(String[] args) {
         try {
             Dev dev = new Dev();
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter dice");
-            String firstRoll = scanner.nextLine();
-            RollerLogic.mainProcessLoop(firstRoll, dev, scanner);
-            dev.close();
+
+            Javalin app = Javalin.create().start(8080);
+
+            app.get("/roll", ctx -> {
+                SumOfAllDices.list.clear(); // ← dodaj ovo
+                String dice = ctx.queryParam("dice");
+                DiceDecoder.validateAndInitializeDiceRoll(dice, dev);
+                ctx.result("OK");
+            });
+
         } catch (SQLException e) {
             System.out.println("Database error: " + e.getMessage());
         }
     }
 
-//    1. Napraviti loop koji ce da tera ceo flow programa
-//    2. napraviti bazu sa rollovima
+//    public static void main(String[] args) {
+//        try {
+//            Dev dev = new Dev();
+//            Scanner scanner = new Scanner(System.in);
+//            System.out.println("Enter dice");
+//            String firstRoll = scanner.nextLine();
+//            RollerLogic.mainProcessLoop(firstRoll, dev, scanner);
+//            dev.close();
+//        } catch (SQLException e) {
+//            System.out.println("Database error: " + e.getMessage());
+//        }
+//    }
+
 
 }
